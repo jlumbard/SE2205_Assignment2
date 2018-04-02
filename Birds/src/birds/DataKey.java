a2e193fd4c9814272172eef5014795686a51547d
kevinliu123 <kliu265@uwo.ca>
2018-04-02 15:00:19 -0400

    Created Node class and updated DataKey class

--- Birds/src/birds/DataKey.java
+++ Birds/src/birds/DataKey.java
@@ -2,20 +2,39 @@ package birds;
 
 public class DataKey {
 
-    
+    String birdName;
+    int birdSize;
 	// default constructor
-	public DataKey() {
-	 
+	public DataKey(int size, String name) {
+	 this.birdName = name;
+         this.birdSize = size;
 	}
+        
+        public String getBirdName(){
+        return this.birdName;
+        }
+        
+        public int getBirdSize(){
+        return this.birdSize;
+        }
         
 	// other constructors
 
 	/**
 	 * Returns 0 if this DataKey is equal to k, returns -1 if this DataKey is smaller
 	 * than k, and it returns 1 otherwise. 
+     * @param k
+     * @return 
 	 */
 	public int compareTo(DataKey k) {
+           if (this.birdName.equalsIgnoreCase(k.birdName) && this.birdSize == k.birdSize){
            return 0;
-            
+           }  
+           else if (this.birdSize < k.birdSize ||(this.birdSize == k.birdSize && this.birdName.compareTo(k.birdName)<0)){
+           return -1;
+           }
+           else {
+           return 1;
+           }
 	}
 }
---
+++ Birds/src/birds/Node.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package birds;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Node {
+    int key;
+    String name;
+    
+    Node leftChild;
+    Node rightChild;
+    
+    Node(int key, String name){
+    this.key = key;
+    this.name = name;
+    }
+    
+    public String toString(){
+        return name + " has a key "+ key;
+    }
+}
